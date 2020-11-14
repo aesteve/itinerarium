991f1a783776a87ed4c6d1f8b42090057cc8bd3c
Arnaud Esteve <arnaud.esteve@gmail.com>
2020-11-14 17:51:28 +0100

    Clean SSL field

--- src/conf/api.rs
+++ src/conf/api.rs
@@ -1,4 +1,4 @@
-use crate::conf::endpoint::{Endpoints};
+use crate::conf::endpoint::{HttpEndpoint};
 use std::string::ParseError;
 use hyper::{Request, Body, Response, Error};
 use hyper::http::uri::PathAndQuery;
@@ -6,32 +6,32 @@ use hyper::http::uri::PathAndQuery;
 
 #[derive(Debug, Clone)]
 pub(crate) struct Api {
-    pub(crate) endpoints: Vec<Endpoints>,
+    pub(crate) endpoints: Vec<HttpEndpoint>,
     pub(crate) prefix: String,
 }
 
 impl Api  {
     pub(crate) fn http(host: &str, port: u16, prefix: String) -> Result<Self, ParseError> {
         Ok(Api {
-            endpoints: vec![Endpoints::http(host, port)?],
+            endpoints: vec![HttpEndpoint::http(host, port)?],
             prefix
         })
     }
 
     pub(crate) fn https(host: &str, prefix: String) -> Result<Self, ParseError> {
         Ok(Api {
-            endpoints: vec![Endpoints::https(host)?],
+            endpoints: vec![HttpEndpoint::https(host)?],
             prefix
         })
     }
 
     pub(crate) async fn forward(&self, mut req: Request<Body>) -> Result<Response<Body>, Error> {
         match self.endpoint_for(&req) {
-            Endpoints::Plain(e) => {
+            HttpEndpoint::Plain(e) => {
                 self.mut_req(&mut req);
                 e.client.clone().request(req).await
             },
-            Endpoints::Ssl(e) => {
+            HttpEndpoint::Ssl(e) => {
                 self.mut_req(&mut req);
                 e.client.clone().request(req).await
             }
@@ -43,12 +43,12 @@ impl Api  {
         // TODO: gateway headers (X-Forwarded-For, etc.)
         let path = build_path(req.uri().path_and_query(), self.prefix.len());
         *req.uri_mut() = match self.endpoint_for(req) {
-            Endpoints::Plain(e) => format!(
+            HttpEndpoint::Plain(e) => format!(
                 "http://{}{}",
                 e.address.clone(),
                 path
             ),
-            Endpoints::Ssl(e) => format!(
+            HttpEndpoint::Ssl(e) => format!(
                 "https://{}{}",
                 e.address.clone(),
                 path
@@ -60,8 +60,9 @@ impl Api  {
         req.uri().path().starts_with(&self.prefix)
     }
 
-    pub(crate) fn endpoint_for(&self, _req: &Request<Body>) -> &Endpoints {
-        self.endpoints.get(0).unwrap() // TODO: decision tree (health checks, etc.)
+    pub(crate) fn endpoint_for(&self, _req: &Request<Body>) -> &HttpEndpoint {
+        // TODO: decision tree (based on health checks, response times, etc.)
+        self.endpoints.get(0).unwrap()
     }
 }
 
--- src/conf/endpoint.rs
+++ src/conf/endpoint.rs
@@ -5,7 +5,7 @@ use hyper_tls::HttpsConnector;
 
 
 #[derive(Debug, Clone)]
-pub(crate) enum Endpoints {
+pub(crate) enum HttpEndpoint {
     Plain(Endpoint<HttpConnector>),
     Ssl(Endpoint<HttpsConnector<HttpConnector>>)
 }
@@ -13,30 +13,27 @@ pub(crate) enum Endpoints {
 #[derive(Debug, Clone)]
 pub(crate) struct Endpoint<T> {
     pub(crate) address: String,
-    pub(crate) ssl: bool,
     pub(crate) client: Client<T>,
 }
 
-impl Endpoints {
+impl HttpEndpoint {
 
     pub(crate) fn http(host: &str, port: u16) -> Result<Self, ParseError> {
-        Ok(Endpoints::Plain(Endpoint {
+        Ok(HttpEndpoint::Plain(Endpoint {
             address: format!("{}:{}", host, port),
-            ssl: false,
-            client: Client::builder()
-                .build_http(), // TODO: configure client according to endpoint conf (retry / timeout / etc.)
+            client: Client::builder() // TODO: configure client according to endpoint conf (retry / timeout / etc.)
+                .build_http(),
         }))
     }
 
     pub(crate) fn https(address: &str) -> Result<Self, ParseError> {
         let connector = HttpsConnector::new();
+        // TODO: configure client according to endpoint conf (retry / timeout / etc.)
         let client = Client::builder()
         .build(connector);
-        Ok(Endpoints::Ssl(Endpoint {
+        Ok(HttpEndpoint::Ssl(Endpoint {
             address: address.to_string(),
-            ssl: true,
-            client, // TODO: configure client according to endpoint conf (retry / timeout / etc.)
-
+            client,
         }))
     }
 
